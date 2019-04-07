ea4036fd49391d50333fae30905cf13914f7c815
Zafer Celenk <zafercelenk@gmail.com>
2019-04-07 21:26:30 +0300

    Parola karıştırması için Argon2 kullanımına geçildi.

--- controllers/default.go
+++ controllers/default.go
@@ -1,40 +1,47 @@
 package controllers
 
 import (
-	"github.com/astaxie/beego"
 	"kartvizit/models"
 	"strings"
+
+	"github.com/astaxie/beego"
 )
 
 type MainController struct {
 	BaseController
 }
 
+// Get function
 func (c *MainController) Get() {
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
 	c.TplName = "index.tpl"
 }
 
+// Login function
 func (c *MainController) Login() {
 	if c.Ctx.Input.IsPost() {
 		var email = strings.TrimSpace(c.GetString("email"))
 		var password = strings.TrimSpace(c.GetString("password"))
 		beego.Info(email)
 		var flash = beego.NewFlash()
+
+		hash, _ := models.GenerateFromPassword(password)
+		beego.Info("----> ", string(hash))
+
 		if email != "" && password != "" {
 			user, err := models.GetUserByEmail(email)
 			beego.Info(user)
 			beego.Info(err)
-			beego.Info(models.ComparePasswords(user.Password, password))
 
-			if err != nil || !models.ComparePasswords(user.Password, password) {
-				flash.Error(c.Tr("loginErrorWrong"))
-				flash.Store(&c.Controller)
-			} else {
+			match, err := models.ComparePasswordAndHash(password, user.Password)
+			if err == nil && match {
 				c.SetSession("isLogin", true)
 				c.UserID = user.ID
 				c.Redirect("/", 302)
+			} else {
+				flash.Error(c.Tr("loginErrorWrong"))
+				flash.Store(&c.Controller)
 			}
 		} else {
 			beego.Info("empty")
--- models/user.go
+++ models/user.go
@@ -1,24 +1,43 @@
 package models
 
 import (
+	"crypto/rand"
+	"crypto/subtle"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"strings"
 	"time"
 
-	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
-	"golang.org/x/crypto/bcrypt"
+	"golang.org/x/crypto/argon2"
 )
 
-// User struct
-type User struct {
-	ID       int       `orm:"auto;column(id)"`
-	Email    string    `orm:"size(150)"`
-	Password string    `orm:"size(150)"`
-	Name     string    `orm:"size(150)"`
-	Surname  string    `orm:"size(150)"`
-	Created  time.Time `orm:"auto_now_add;type(datetime)"`
-	Modified time.Time `orm:"auto_now;type(datetime)"`
-	Cards    []*Card   `orm:"reverse(many)"`
-}
+var (
+	ErrInvalidHash         = errors.New("the encoded hash is not in the correct format")
+	ErrIncompatibleVersion = errors.New("incompatible version of argon2")
+)
+
+type (
+	User struct {
+		ID       int       `orm:"auto;column(id)"`
+		Email    string    `orm:"size(150)"`
+		Password string    `orm:"size(150)"`
+		Name     string    `orm:"size(150)"`
+		Surname  string    `orm:"size(150)"`
+		Created  time.Time `orm:"auto_now_add;type(datetime)"`
+		Modified time.Time `orm:"auto_now;type(datetime)"`
+		Cards    []*Card   `orm:"reverse(many)"`
+	}
+
+	params struct {
+		memory      uint32
+		iterations  uint32
+		parallelism uint8
+		saltLength  uint32
+		keyLength   uint32
+	}
+)
 
 func init() {
 	orm.RegisterModel(new(User))
@@ -28,6 +47,7 @@ func (u *User) TableName() string {
 	return "users"
 }
 
+// GetUserByEmail function
 func GetUserByEmail(email string) (*User, error) {
 	o := orm.NewOrm()
 	o.Using("default")
@@ -42,23 +62,90 @@ func GetUserByEmail(email string) (*User, error) {
 	return &user, nil
 }
 
-func HashAndSalt(pwd string) string {
-	hash, err := bcrypt.GenerateFromPassword([]byte(pwd), bcrypt.MinCost)
-	if err != nil {
-		beego.Debug(err)
+// GenerateFromPassword function
+func GenerateFromPassword(password string) (encodedHash string, err error) {
+	var p = params{
+		memory:      64 * 1024,
+		iterations:  3,
+		parallelism: 2,
+		saltLength:  16,
+		keyLength:   32,
 	}
 
-	return string(hash)
+	salt, err := generateRandomBytes(p.saltLength)
+	if err != nil {
+		return "", err
+	}
+
+	var hash = argon2.IDKey([]byte(password), salt, p.iterations, p.memory,
+		p.parallelism, p.keyLength)
+
+	b64Salt := base64.RawStdEncoding.EncodeToString(salt)
+	b64Hash := base64.RawStdEncoding.EncodeToString(hash)
+
+	encodedHash = fmt.Sprintf("$argon2id$v=%d$m=%d,t=%d,p=%d$%s$%s",
+		argon2.Version, p.memory, p.iterations, p.parallelism, b64Salt, b64Hash)
+
+	return encodedHash, nil
 }
 
-func ComparePasswords(hashedPwd string, plainPwd string) bool {
-	byteHash := []byte(hashedPwd)
-
-	err := bcrypt.CompareHashAndPassword(byteHash, []byte(plainPwd))
+// ComparePasswordAndHash function
+func ComparePasswordAndHash(password, encodedHash string) (match bool, err error) {
+	p, salt, hash, err := decodeHash(encodedHash)
 	if err != nil {
-		beego.Debug(err)
-		return false
+		return false, err
 	}
 
-	return true
+	otherHash := argon2.IDKey([]byte(password), salt, p.iterations, p.memory, p.parallelism, p.keyLength)
+
+	if subtle.ConstantTimeCompare(hash, otherHash) == 1 {
+		return true, nil
+	}
+	return false, nil
+}
+
+func decodeHash(encodedHash string) (p *params, salt, hash []byte, err error) {
+	vals := strings.Split(encodedHash, "$")
+	if len(vals) != 6 {
+		return nil, nil, nil, ErrInvalidHash
+	}
+
+	var version int
+	_, err = fmt.Sscanf(vals[2], "v=%d", &version)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	if version != argon2.Version {
+		return nil, nil, nil, ErrIncompatibleVersion
+	}
+
+	p = &params{}
+	_, err = fmt.Sscanf(vals[3], "m=%d,t=%d,p=%d", &p.memory, &p.iterations, &p.parallelism)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	salt, err = base64.RawStdEncoding.DecodeString(vals[4])
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	p.saltLength = uint32(len(salt))
+
+	hash, err = base64.RawStdEncoding.DecodeString(vals[5])
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	p.keyLength = uint32(len(hash))
+
+	return p, salt, hash, nil
+}
+
+func generateRandomBytes(n uint32) ([]byte, error) {
+	var b = make([]byte, n)
+	var _, err = rand.Read(b)
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
 }
